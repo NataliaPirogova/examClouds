0d74b280abc8fe99c21000667a500a9919623f00
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-06-19 15:32:21 +0300

    T2 done

---
+++ src/com/tasks/lesson3/Task2.java
@@ -0,0 +1,21 @@
+package com.tasks.lesson3;
+
+public class Task2 {
+    public static void main (String[] args){
+    boolean bool = true;
+    System.out.println(bool);
+    String str = "Hi!";
+        System.out.println(str);
+        char ch = 3400;
+        System.out.println(ch);
+        int i1 = 0b10;
+        int i2 = 027;
+        int i3 = 122;
+        int i4 = 0b10;
+        System.out.println(i1+", "+i2+", "+i3+", "+i4);
+        float f = 2.933F;
+        System.out.println(f);
+        double d = 10.3134555;
+        System.out.println(d);
+    }
+}
