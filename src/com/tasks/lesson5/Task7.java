7e27e5339ad8a806588a9e1e11bf58d084c05d00
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-07-02 20:29:07 +0300

    T7 done

---
+++ src/com/tasks/lesson5/Task7.java
@@ -0,0 +1,10 @@
+package com.tasks.lesson5;
+
+public class Task7 {
+    public static void main(String[] args) {
+        char letter = 'h';
+        for (int i = 0; i < 5; i++) {
+            System.out.print(letter++ + " ");
+        }
+    }
+}
