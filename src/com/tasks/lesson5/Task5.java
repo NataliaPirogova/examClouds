af06e2da862783f67e882277687843d7fb9a51d9
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-07-02 20:18:41 +0300

    T5 done

---
+++ src/com/tasks/lesson5/Task5.java
@@ -0,0 +1,11 @@
+package com.tasks.lesson5;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("Task" + i);
+            i++;
+        }
+    }
+}
