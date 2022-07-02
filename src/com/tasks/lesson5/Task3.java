8ac30250aa3b82362b94f501a43b5b6c6fbcd545
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-07-02 20:16:40 +0300

    T4 done

--- src/com/tasks/lesson5/Task3.java
+++ src/com/tasks/lesson5/Task3.java
@@ -2,21 +2,19 @@ package com.tasks.lesson5;
 
 public class Task3 {
     public static void main(String[] args) {
-        if("1".equals(args[0])){
+        if ("1".equals(args[0])) {
             System.out.println("Понедельник");
-        }
-       else if("2".equals(args[0])){
+        } else if ("2".equals(args[0])) {
             System.out.println("Вторник");
-        }else if("3".equals(args[0])){
+        } else if ("3".equals(args[0])) {
             System.out.println("Среда");
-        }else if("4".equals(args[0])){
+        } else if ("4".equals(args[0])) {
             System.out.println("Четверг");
-        }else if("5".equals(args[0])){
+        } else if ("5".equals(args[0])) {
             System.out.println("Пятница");
-        }else if("6".equals(args[0]) || "7".equals(args[0])){
+        } else if ("6".equals(args[0]) || "7".equals(args[0])) {
             System.out.println("Выходной");
-        }
-       else{
+        } else {
             System.out.println("Error");
         }
     }
