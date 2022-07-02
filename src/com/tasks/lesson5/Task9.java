ad0bcba2eeaba7f1d4592de6c11d3d1576f39c3f
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-07-02 19:59:04 +0300

    T9 done

--- src/com/tasks/lesson5/Task9.java
+++ src/com/tasks/lesson5/Task9.java
@@ -2,6 +2,12 @@ package com.tasks.lesson5;
 
 public class Task9 {
     public static void main(String[] args) {
-
+        int n = (int) (5 * Math.random());
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println("Наше случайное число: " + n);
+        System.out.println("Факториал числа: " + factorial);
     }
 }
