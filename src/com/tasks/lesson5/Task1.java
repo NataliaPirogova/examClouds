ac0b0fbd92b88bb39222b72fd9685eb700dbbd40
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-07-02 19:47:49 +0300

    T1 done

--- src/com/tasks/lesson5/Task1.java
+++ src/com/tasks/lesson5/Task1.java
@@ -1,17 +1,15 @@
 package com.tasks.lesson5;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Task1 {
-//    public static void main(String[] args) {
-//        public void method1 (String y){
-//            int x = Integer.parseInt(y);
-//            if (x % 2 == 1) {
-//                System.out.println(x);
-//            } else {
-//                System.out.println("число четное");
-//            }
-//        }
-////        main("2");
-//    }
-
-
+    public static void main(String @NotNull [] args) {
+        int x = Integer.parseInt(args[0]);
+        if (x % 2 == 1) {
+            System.out.println(x);
+        } else {
+            System.out.println("число четное");
+        }
+    }
 }
+
---
+++ src/com/tasks/lesson5/Task9.java
@@ -0,0 +1,7 @@
+package com.tasks.lesson5;
+
+public class Task9 {
+    public static void main(String[] args) {
+
+    }
+}
