f884b952c097f9c0bd71994eadc133b2cf4da42f
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-08-01 23:00:50 +0300

    T6 L18 done

--- src/com/tasks/lesson18/Task1.java
+++ src/com/tasks/lesson18/Task1.java
@@ -2,8 +2,8 @@ package com.tasks.lesson18;
 
 import com.tasks.lesson17.Printable;
 
-public class Task1 {
-    public static void main(String[] args) {
-//        Printable printable=()->print();
-    }
-}
+//public class Task1 {
+//    public static void main(String[] args) {
+//        Printable printable=(String s)->print(s);
+//    }
+//}
